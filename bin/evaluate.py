89043225589fd3f7b9e5b45a4151998c5a1e1e42
Yuqun Wu <yuqunwu2@vision-12.cs.illinois.edu>
2021-12-23 15:15:09 -0600

    add_network

--- bin/evaluate.py
+++ bin/evaluate.py
@@ -122,15 +122,9 @@ if __name__ == '__main__':
 
     #########################################################################################################
     # Get network, need manual modification
-
-    # from model.normal_old_estimator import NormalEstimator
-    # n_model = NormalEstimator(pretrained_weight = 'weights/ori_weight/normal_scannet.pth').to(dev)
-    # n_model.eval()
-    
-    sys.path.append('../surface_normal_uncertainty')
     from models.NNET import NNET
     from utils import utils
-    checkpoint = '../surface_normal_uncertainty/checkpoints/%s.pt' % args.pretrained
+    checkpoint = 'checkpoints/%s.pt' % args.pretrained
     model = NNET(args).to(dev)
     model = utils.load_checkpoint(checkpoint, model)
     model.eval()
