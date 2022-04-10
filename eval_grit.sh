fd4d087e3ac482646622309dcb5cf091ba62c7c0
Ryan Marten <ryanmarten2000@gmail.com>
2022-04-10 15:13:43 -0500

    remove notebook checkpoints

---
+++ eval_grit.sh
@@ -0,0 +1,5 @@
+python bin/evaluate.py \
+--results_dir results/images \
+--log_path results/evaluation \
+--data_dir /scratch/marten4/grit_official_data/GRIT/images \
+--dataset BlendedMVS DTU
---
+++ setup.sh
@@ -0,0 +1,4 @@
+# download checkpoint models (download.py doesn't work anymore)
+mkdir './checkpoints'
+~/downloads/gdrive download 1RNiYw5rrqgBf3OkFSCSSQ67s0HMBpkAv --path ./checkpoints/
+~/downloads/gdrive download 1lOgY9sbMRW73qNdJze9bPkM2cmfA8Re- --path ./checkpoints/
